3b6b1df1d3057987a91a2eb07404ca55e3fa6792
Gyxius <mitsou.fortunat@gmail.com>
2025-07-31 19:56:52 +0200

    Update model pkl to json

--- load_model.py
+++ load_model.py
@@ -1,18 +1,12 @@
 import numpy as np
-import pickle
-from keras.utils import pad_sequences
+import json
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
 from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import pad_sequences  # you can remove if already imported elsewhere
 
-def dummy_npwarn_decorator_factory():
-  def npwarn_decorator(x):
-    return x
-  return npwarn_decorator
-np._no_nep50_warning = getattr(np, '_no_nep50_warning', dummy_npwarn_decorator_factory)
+# Load tokenizer from JSON
+with open("tokenizer.json", "r", encoding="utf-8") as f:
+    tokenizer = tokenizer_from_json(f.read())
 
-
-
-with open("tokenizer.pkl", "rb") as f:
-    tokenizer = pickle.load(f)
-
+# Load model
 model = load_model("my_model.keras")
-
