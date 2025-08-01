ed20bc03e67870c941e48e98305fa673c9b88e79
Gyxius <mitsou.fortunat@gmail.com>
2025-08-01 16:14:41 +0200

    remove the model for the moment

--- model.py
+++ model.py
@@ -1,11 +1,13 @@
-from transformers import pipeline
+# from transformers import pipeline
 
-classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+# # classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+# classifier = pipeline("sentiment-analysis", model="Seethal/sentiment_analysis_generic_dataset")
 
-def predict_sentiment(sample_text = "The movie was terrible, i will not recommend this movie to anyone"):
-    # Don't forget to tokenize & pad!
-    texts = [sample_text]
-    results = classifier(texts)
 
-    for text, result in zip(texts, results):
-        return result['label']
+# def predict_sentiment(sample_text = "The movie was terrible, i will not recommend this movie to anyone"):
+#     # Don't forget to tokenize & pad!
+#     texts = [sample_text]
+#     results = classifier(texts)
+
+#     for text, result in zip(texts, results):
+#         return result['label']
